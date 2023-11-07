9a1561090132c6f3720aa227be2c0ac8ccfc0346
Matias Fiorentini <mati.fiore1996@gmail.com>
2023-11-06 22:26:40 -0300

    first commit

---
+++ src/main/java/Copia/Semillero/SemilleroLatam/SemilleroLatamApplication.java
@@ -0,0 +1,13 @@
+package Copia.Semillero.SemilleroLatam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemilleroLatamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemilleroLatamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Copia/Semillero/SemilleroLatam/SemilleroLatamApplicationTests.java
@@ -0,0 +1,13 @@
+package Copia.Semillero.SemilleroLatam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemilleroLatamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
